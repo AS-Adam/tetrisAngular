2034f4fad38fa76e8360cdf8c4c491d4a89736ed
Adam Sulewski <a.sulewski1@gmail.com>
2021-02-18 22:18:49 +0100

    functions and css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,42 +13,66 @@ export class AppComponent {
   time: number = 0;
   counter: number = 0;
   status: string = "ready";
-
+  interval;
   add() {
     if (this.name.length > 0 && this.email.length > 0) {
       this.playerInfo.push(this.name);
     }
-
   }
-
-  showTime() {
-    setInterval(() => { this.time++ }, 1000)
+  showTime(stat) {
+    switch (stat) {
+      case 'started':
+        this.interval = setInterval(() => {
+          this.time++;
+        }, 1000);
+        break;
+      case 'stopped':
+        // this.time = 0;
+        clearInterval(this.interval);
+        break;
+      case 'ready':
+        this.time = 0;
+        clearInterval(this.interval);
+        break;
+      // case 'restart':
+      //   this.time = 0;
+      //   this.status = "started";
+      //   break;
+    };
   }
-
   startGame(game) {
     game.actionStart();
-    this.showTime();
-    this.status = "started"
+    if (this.status != "started") {
+      this.status = "started";
+      this.showTime(this.status);
+    }
   }
   pauseGame(game) {
+    this.status = "stopped";
     game.actionStop();
-    this.status = "stopped"
+    this.showTime(this.status);
   }
   gameCounter() {
     this.counter++;
   }
-
   endGame(game) {
-    console.log("wyłączyło")
     game.actionStop();
     this.playerInfo = [];
     this.name = "";
     this.email = "";
     this.counter = 0;
     this.status = "ready"
-
+    this.showTime(this.status);
   }
+  restartGame(game) {
+    game.actionStop();
+    this.startGame(game);
+    game.actionReset();
+    this.time = 0;
+    this.counter = 0;
 
+    // this.showTime(this.status)
+  }
 }
 
 
